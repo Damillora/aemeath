cba44d75831f9d5f04c21189fd7714ed53c2ab2d
Damillora <damillora@damillora.com>
2026-05-06 20:17:07 +0700

    chore: update build script

--- .github/scripts/complete-build.sh
+++ .github/scripts/complete-build.sh
@@ -5,6 +5,7 @@ set -euox pipefail
 .github/scripts/buildstream-conf.sh > $HOME/.config/buildstream.conf
 .github/scripts/generate-version.sh
 source <(yq -o=shell include/image-version.yml)
+just clean
 just build
 bst build components/boot/linux.bst
 .github/scripts/buildstream-conf.sh nopush > $HOME/.config/buildstream.conf
@@ -32,6 +33,18 @@ cat > "CHANGELOG.md" << EOF
 
 EOF
 git cliff --unreleased --tag "$image_version" --strip header >> CHANGELOG.md
+for f in target/*
+do
+  rclone copy "$f" fina:aemeath/sysupdate/
+done
+for f in disks/*
+do
+  rclone copy "$f" fina:aemeath/images/
+done
+for f in live/*
+do
+  rclone copy "$f" fina:aemeath/images/
+done
 git tag "$image_version"
 git push origin tag "$image_version"
 gh release create ${image_version} -F CHANGELOG.md
